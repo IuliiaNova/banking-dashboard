2237768bf5300f36b1f2186b9cfbdd554bbb4326
Iuliia Shikhanova <yshihanova@gmail.com>
2025-05-30 01:38:13 +0100

    feat(dashboard): add visual options

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,43 @@
+import { CirclePlus, CreditCard, HandCoins } from "lucide-react";
 import { AccountOverview } from "./components/AccountOverview";
+import FinancialUnit from "./components/FinancialUnit";
+import { useTransactions } from "../../features/transactions/store/transactions.context";
 
 function Dashboard() {
+  const {
+    state: { transactions },
+  } = useTransactions("AccountOverview");
+  const balance = transactions.reduce((sum, t) => sum + t.amount, 0);
   return (
-    <div className="">
+    <div className="space-y-5">
       <AccountOverview />
+      <div className="flex gap-6 justify-between">
+        <FinancialUnit
+          title="Account"
+          code="1234567812365985"
+          type="account"
+          icon={<HandCoins />}
+          amount={balance}
+        />
+        <FinancialUnit
+          title="Credit Card"
+          code="1234567812345678"
+          type="card"
+          status="Active"
+          icon={<CreditCard />}
+        />
+      </div>
+      
+      <div className="flex flex-col gap-2">
+        <span className="mb-4 text-md font-semibold text-gray-800 dark:text-gray-100 truncate">
+        Funds and investments
+      </span>
+        <div className="w-44 sm:w-80 md:w-96 bg-white dark:bg-background-dark rounded-2xl shadow-md p-4 flex flex-col gap-3 transition-all duration-300">
+          <CirclePlus size={40} color="gray" />
+          <span className="text-sm font-semibold text-gray-800 dark:text-gray-100 truncate">
+            Add funds </span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/dashboard/components/FinancialUnit.tsx
+++ src/pages/dashboard/components/FinancialUnit.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
 
-export default function FinancialUnit() {
+type FinancialUnitProps = {
+  title: string;
+  code: string;
+  amount?: number;
+  type: "card" | "account" | string;
+  status?: string;
+  icon?: React.ReactNode;
+};
+
+export default function FinancialUnit({
+  title,
+  code,
+  amount,
+  type,
+  status,
+  icon,
+}: FinancialUnitProps) {
+  const lastFourDigits = code.slice(-4);
+
   return (
-    <div>FinancialUnit</div>
-  )
+    <div className="w-44 sm:w-80 md:w-96 bg-white dark:bg-background-dark rounded-2xl shadow-md p-4 flex flex-col gap-3 transition-all duration-300">
+      <div className="flex items-center gap-3">
+        {icon && <div className="text-xl text-rose-base">{icon}</div>}
+        <span className="text-sm font-semibold text-gray-800 dark:text-gray-100 truncate">
+          {title}
+        </span>
+      </div>
+
+      <div className="text-gray-500 dark:text-gray-400 text-sm tracking-widest">
+        •••• {lastFourDigits}
+      </div>
+
+      {type === "card" ? (
+        <div className="text-sm font-medium text-blue-600 dark:text-green-600 flex gap-2 items-center">
+          <div className="w-3 h-3 rounded-full bg-green-600"/>
+          <span>{status || "Stateless"}</span>
+        </div>
+      ) : (
+        <div className="text-lg font-bold text-blsck dark:text-gray-200">
+          {amount != null ? `${amount.toLocaleString()} €` : "0 €"}
+        </div>
+      )}
+    </div>
+  );
 }
