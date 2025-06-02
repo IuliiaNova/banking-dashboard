8380b9edeb945a4b1b9f6d383ae94bfe9b8ae4dd
Iuliia Shikhanova <yshihanova@gmail.com>
2025-06-02 13:06:33 +0100

    fix: spaces

--- src/features/login/context/auth.provider.tsx
+++ src/features/login/context/auth.provider.tsx
@@ -1,12 +1,9 @@
-import React, { useState, useEffect, type ReactNode } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, type ReactNode } from "react";
 import { AuthContext } from "./auth.context";
-
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
 
   return (
     <AuthContext.Provider value={{ username, password, setUsername, setPassword }}>
