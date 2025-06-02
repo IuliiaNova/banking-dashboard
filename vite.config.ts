5a0eabb891db6aad185ff667ad00c8447f5e5741
Iuliia Shikhanova <yshihanova@gmail.com>
2025-06-02 21:54:57 +0100

    test ghpage

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/banking-dashboard/',
   test: {
     globals: true,
     environment: 'jsdom',
